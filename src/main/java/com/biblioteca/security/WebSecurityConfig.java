de84c7192ee7536af6bd239a558853f169d8728f
William Andreoli <willandreoli33@gmail.com>
2024-10-30 15:04:32 -0300

    Integracao com o Swagger

--- src/main/java/com/biblioteca/security/WebSecurityConfig.java
+++ src/main/java/com/biblioteca/security/WebSecurityConfig.java
@@ -24,61 +24,63 @@ public class WebSecurityConfig {
 
 	@Autowired
 	private AuthEntryPointJwt unauthorizedHandler;
-	
+
 	@Bean
 	public PasswordEncoder passwordEnconder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
-	
+
 	@Bean
 	public AuthFilterToken authFilterToken() {
 		return new AuthFilterToken();
 	}
-	
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
+
 		http.cors(Customizer.withDefaults());
 		http.csrf(csrf -> csrf.disable())
-			.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
-							.requestMatchers(HttpMethod.POST,"/usuarios/**").permitAll()
-							.requestMatchers(HttpMethod.GET,"/usuarios/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.POST,"/usuarios/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.PUT,"/usuarios/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.DELETE,"/usuarios/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.GET, "/livros/**").permitAll()
-							.requestMatchers(HttpMethod.POST, "/livros/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.PUT, "/livros/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.DELETE, "/livros/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.GET, "/editoras/**").permitAll()
-							.requestMatchers(HttpMethod.POST, "/editoras/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.PUT, "/editoras/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.DELETE, "/editoras/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.GET, "/autores/**").permitAll()
-							.requestMatchers(HttpMethod.POST, "/autores/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.PUT, "/autores/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.DELETE, "/autores/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.GET, "/areas/**").permitAll()
-							.requestMatchers(HttpMethod.POST, "/areas/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.PUT, "/areas/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.DELETE, "/areas/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.POST, "/emprestimos/**").permitAll()
-							.requestMatchers(HttpMethod.GET, "/emprestimos/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.DELETE, "/emprestimos/**").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.PUT, "/emprestimos/**").hasRole("ADMIN")
-							.anyRequest().authenticated());
-		
+				.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/v3/api-docs/**").permitAll()
+						.requestMatchers("/swagger-ui/**").permitAll().requestMatchers("/swagger-ui.html").permitAll()
+						.requestMatchers("/auth/**").permitAll().requestMatchers(HttpMethod.POST, "/usuarios/**")
+						.permitAll().requestMatchers(HttpMethod.GET, "/usuarios/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.POST, "/usuarios/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.PUT, "/usuarios/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.DELETE, "/usuarios/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.GET, "/livros/**").permitAll()
+						.requestMatchers(HttpMethod.POST, "/livros/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.PUT, "/livros/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.DELETE, "/livros/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.GET, "/editoras/**").permitAll()
+						.requestMatchers(HttpMethod.POST, "/editoras/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.PUT, "/editoras/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.DELETE, "/editoras/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.GET, "/autores/**").permitAll()
+						.requestMatchers(HttpMethod.POST, "/autores/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.PUT, "/autores/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.DELETE, "/autores/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.GET, "/areas/**").permitAll()
+						.requestMatchers(HttpMethod.POST, "/areas/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.PUT, "/areas/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.DELETE, "/areas/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.POST, "/emprestimos/**").permitAll()
+						.requestMatchers(HttpMethod.GET, "/emprestimos/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.DELETE, "/emprestimos/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.PUT, "/emprestimos/**").hasRole("ADMIN").anyRequest()
+						.authenticated());
+
 		http.addFilterBefore(authFilterToken(), UsernamePasswordAuthenticationFilter.class);
-		
+
 		return http.build();
-		
+
 	}
-	
+
 }
